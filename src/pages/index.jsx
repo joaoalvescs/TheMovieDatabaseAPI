79be8c3fb3abe8a02eab9b4c13e997771124c5bd
João Alves <joaoalves.cs@gmail.com>
2021-12-27 16:30:18 -0300

    Listando os filmes mais populares

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,28 @@
-function Home() {
+import React, { useState, useEffect } from 'react'
+
+function App() {
+  const [movie, setMovie] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=5e3331f5003efc4ef18f2a1a4699927b&language=pt-BR&page=1`,
+      );
+      const json = await res.json()
+      setMovie(json.results)
+    };
+    fetchData();
+  });
+
   return (
     <ul>
-      Olá mundo!
+      {movie.map(item => (
+        <li>
+          <a> {item.title} </a>
+        </li>
+      ))}
     </ul>
   );
 }
 
-export default Home;
+export default App
